7add7488ad4f52b371770d7faa0b604c3b3fc8ec
guer0157 <32390693+guer0157@users.noreply.github.com>
2018-11-28 00:38:45 -0500

    Initial Commit

---
+++ iosFinal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosFinal
+//
+//  Created by Cesar Guerrero on 2018-11-28.
+//  Copyright © 2018 guer0157.AlgonquinCollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
