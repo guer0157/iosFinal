aed33a1545f9ae9c910449e52150bbd836aa45ed
guer0157 <32390693+guer0157@users.noreply.github.com>
2018-11-28 01:00:10 -0500

    base

---
+++ iosFinal/AddViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddViewController.swift
+//  iosFinal
+//
+//  Created by Cesar Guerrero on 2018-11-28.
+//  Copyright © 2018 guer0157.AlgonquinCollege. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
